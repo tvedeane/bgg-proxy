e369dba51fb34daf30b7c7dd81fb146a9e37632b
tvedeane <1138746+tvedeane@users.noreply.github.com>
2025-01-02 16:54:18 +0100

    Add streaming endpoint

--- src/main/java/dev/tvedeane/BoardgameController.java
+++ src/main/java/dev/tvedeane/BoardgameController.java
@@ -1,6 +1,5 @@
 package dev.tvedeane;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Lists;
@@ -10,27 +9,68 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.reactivestreams.Publisher;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller("boardgames")
 public class BoardgameController {
     private final BoardgamegeekClient boardgamegeekClient;
     private final Cache<String, PlayersCountDto> cache = CacheBuilder.newBuilder()
-            .maximumSize(10_000)
+        .maximumSize(10_000)
 //            .weigher() // TODO use weight based on the game position
-            .build();
+        .build();
+    private final Flowable<PlayersCountDto> oneSecondDelayFlowable =
+        Flowable.timer(1_000, TimeUnit.MILLISECONDS, Schedulers.single()).flatMap(tick -> Flowable.empty());
 
     public BoardgameController(BoardgamegeekClient boardgamegeekClient) {
         this.boardgamegeekClient = boardgamegeekClient;
     }
 
+    @Get(value = "/stream/{ids}", produces = MediaType.APPLICATION_JSON_STREAM)
+    public Publisher<PlayersCountDto> playersCountsStream(String ids) {
+        var separatedIds = Arrays.stream(ids.split(",")).toList();
+        var cached = new ArrayList<PlayersCountDto>();
+        var missingKeys = new ArrayList<String>();
+
+        for (String key : separatedIds) {
+            var value = cache.getIfPresent(key);
+            if (value != null) {
+                cached.add(value);
+            } else {
+                missingKeys.add(key);
+            }
+        }
+
+        Flowable<PlayersCountDto> cachedFlow = Flowable.fromIterable(cached);
+        if (missingKeys.isEmpty()) {
+            return cachedFlow;
+        }
+
+        List<List<String>> partitions = Lists.partition(missingKeys, BoardgamegeekClient.MAX_READING_THINGS);
+        Flowable<PlayersCountDto> uncachedFlow = Flowable.fromIterable(partitions)
+            .concatMap(partition -> {
+                List<PlayersCountDto> fetchedItems = boardgamegeekClient.fetchGame(partition);
+
+                for (var entry : fetchedItems) {
+                    cache.put(entry.id(), entry);
+                }
+
+                return Flowable.fromIterable(fetchedItems).concatWith(oneSecondDelayFlowable);
+            }).subscribeOn(Schedulers.single());
+
+        return Flowable.merge(cachedFlow, uncachedFlow);
+    }
+
     @Get(value = "/{ids}")
     @Produces(MediaType.APPLICATION_JSON)
     @ExecuteOn(TaskExecutors.BLOCKING)
-    public List<PlayersCountDto> playersCounts(String ids) throws JsonProcessingException {
+    public List<PlayersCountDto> playersCounts(String ids) {
         // TODO use regex to check if `ids` has only digits and commas
 
         var separatedIds = Arrays.stream(ids.split(",")).toList();
--- src/test/java/dev/tvedeane/BoardgameControllerTest.java
+++ src/test/java/dev/tvedeane/BoardgameControllerTest.java
@@ -1,7 +1,8 @@
 package dev.tvedeane;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import io.reactivex.rxjava3.core.Flowable;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
 
 import java.util.List;
 
@@ -10,7 +11,42 @@ import static org.mockito.Mockito.*;
 
 class BoardgameControllerTest {
     @Test
-    void usesCacheCallingClientOnce() throws JsonProcessingException {
+    void streamUsesCacheCallingClientOnce() {
+        var mockClient = mock(BoardgamegeekClient.class);
+        var controller = new BoardgameController(mockClient);
+        var e1 = new PlayersCountDto("4", List.of(2), List.of(2));
+        var e2 = new PlayersCountDto("10", List.of(2), List.of(2));
+        when(mockClient.fetchGame(List.of("4", "10"))).thenReturn(List.of(e1, e2));
+
+        var result1 = getBlockingFrom(controller.playersCountsStream("4,10"));
+        var result2 = getBlockingFrom(controller.playersCountsStream("4,10"));
+
+        assertThat(result1).containsOnly(e1, e2);
+        assertThat(result2).containsOnly(e1, e2);
+        verify(mockClient, times(1)).fetchGame(List.of("4", "10"));
+    }
+
+    private static List<PlayersCountDto> getBlockingFrom(Publisher<PlayersCountDto> publisher) {
+        return Flowable.fromPublisher(publisher)
+            .toList()
+            .blockingGet();
+    }
+
+    @Test
+    void partitionsCallsToTheClient() {
+        var mockClient = mock(BoardgamegeekClient.class);
+        var controller = new BoardgameController(mockClient);
+
+        getBlockingFrom(controller.playersCountsStream("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22"));
+
+        verify(mockClient).fetchGame(List.of(
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"));
+        verify(mockClient).fetchGame(List.of("21", "22"));
+    }
+
+    @Test
+    void usesCacheCallingClientOnce() {
         var mock = mock(BoardgamegeekClient.class);
         var controller = new BoardgameController(mock);
         var e1 = new PlayersCountDto("4", List.of(2), List.of(2));
