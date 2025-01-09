c452e20fccfe26f135a56e5834ff42bb0b923531
tvedeane <1138746+tvedeane@users.noreply.github.com>
2025-01-09 11:36:19 +0100

    Remove unused API path

--- src/main/java/dev/tvedeane/BoardgameController.java
+++ src/main/java/dev/tvedeane/BoardgameController.java
@@ -6,9 +6,6 @@ import com.google.common.collect.Lists;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
-import io.micronaut.scheduling.TaskExecutors;
-import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.reactivestreams.Publisher;
@@ -34,6 +31,7 @@ public class BoardgameController {
 
     @Get(value = "/stream/{ids}", produces = MediaType.APPLICATION_JSON_STREAM)
     public Publisher<String> playersCountsStream(String ids) {
+        // TODO use regex to check if `ids` has only digits and commas
         var separatedIds = Arrays.stream(ids.split(",")).toList();
         var cached = new ArrayList<PlayersCountDto>();
         var missingKeys = new ArrayList<String>();
@@ -69,47 +67,5 @@ public class BoardgameController {
 
     private static String addNewline(PlayersCountDto item) {
         return item.toJson() + "\n";
-    }
-
-    @Get(value = "/{ids}")
-    @Produces(MediaType.APPLICATION_JSON)
-    @ExecuteOn(TaskExecutors.BLOCKING)
-    public List<PlayersCountDto> playersCounts(String ids) {
-        // TODO use regex to check if `ids` has only digits and commas
-
-        var separatedIds = Arrays.stream(ids.split(",")).toList();
-        var results = new ArrayList<PlayersCountDto>();
-        var missingKeys = new ArrayList<String>();
-
-        synchronized (cache) { // TODO improve the concurrency
-            for (String key : separatedIds) {
-                var value = cache.getIfPresent(key);
-                if (value != null) {
-                    results.add(value);
-                } else {
-                    missingKeys.add(key);
-                }
-            }
-
-            if (!missingKeys.isEmpty()) {
-                var partitions = Lists.partition(missingKeys, BoardgamegeekClient.MAX_READING_THINGS);
-                partitions.forEach(partition -> {
-                    List<PlayersCountDto> fetchedItems = boardgamegeekClient.fetchGame(partition);
-
-                    for (var entry : fetchedItems) {
-                        cache.put(entry.id(), entry);
-                        results.add(entry);
-                    }
-
-                    try {
-                        // to avoid rate limits
-                        Thread.sleep(1_000); // TODO use better construct
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-        }
-        return results;
     }
 }
--- src/test/java/dev/tvedeane/BoardgameControllerTest.java
+++ src/test/java/dev/tvedeane/BoardgameControllerTest.java
@@ -48,20 +48,4 @@ class BoardgameControllerTest {
             "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"));
         verify(mockClient).fetchGame(List.of("21", "22"));
     }
-
-    @Test
-    void usesCacheCallingClientOnce() {
-        var mock = mock(BoardgamegeekClient.class);
-        var controller = new BoardgameController(mock);
-        var e1 = new PlayersCountDto("4", List.of(2), List.of(2));
-        var e2 = new PlayersCountDto("10", List.of(2), List.of(2));
-        when(mock.fetchGame(List.of("4", "10"))).thenReturn(List.of(e1, e2));
-
-        var result1 = controller.playersCounts("4,10");
-        var result2 = controller.playersCounts("4,10");
-
-        assertThat(result1).containsOnly(e1, e2);
-        assertThat(result2).containsOnly(e1, e2);
-        verify(mock, times(1)).fetchGame(List.of("4", "10"));
-    }
 }
